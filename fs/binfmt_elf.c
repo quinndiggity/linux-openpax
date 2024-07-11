6086e683bc663856e9acbbb1f8816c97663ef06a
Ariadne Conill <ariadne@ariadne.space>
2024-07-10 17:40:01 -0700

    elf: use PAXF_RANDMMAP to control whether ASLR is enabled on a process
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>
    Signed-off-by: Quinn Diggity <64798147+quinndiggitypolymath@users.noreply.github.com>

--- fs/binfmt_elf.c
+++ fs/binfmt_elf.c
@@ -1092,7 +1092,7 @@ out_free_interp:
 	}
 
 	const int snapshot_randomize_va_space = READ_ONCE(randomize_va_space);
-	if (!(current->personality & ADDR_NO_RANDOMIZE) && snapshot_randomize_va_space)
+	if (!(current->personality & ADDR_NO_RANDOMIZE) && test_bit(PAXF_RANDMMAP, &current->mm->pax_flags))
 		current->flags |= PF_RANDOMIZE;
 
 	setup_new_exec(bprm);
