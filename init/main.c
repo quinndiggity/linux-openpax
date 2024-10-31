dd44ac71c97f9327a3f788bc9ebc0918ebc2e3b1
Ariadne Conill <ariadne@ariadne.space>
2024-10-30 22:05:13 -0700

    init: provide pax_softmode symbol unconditionally
    Otherwise we have to add a bunch of #ifdefs when we otherwise
    would not need to.
    
    Fixes: a78588f6fc2f2 ("init: add pax_softmode= kernel command-line option")
    Fixes: https://github.com/edera-dev/linux-openpax/issues/1
    Signed-off-by: Ariadne Conill <ariadne@ariadne.space>
    Signed-off-by: Quinn Diggity <64798147+quinndiggitypolymath@users.noreply.github.com>

--- init/main.c
+++ init/main.c
@@ -190,9 +190,9 @@ static int __init set_reset_devices(char *str)
 
 __setup("reset_devices", set_reset_devices);
 
-#ifdef CONFIG_OPENPAX_SOFTMODE
 int pax_softmode;
 
+#ifdef CONFIG_OPENPAX_SOFTMODE
 static int __init setup_pax_softmode(char *str)
 {
 	get_option(&str, &pax_softmode);
